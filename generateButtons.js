3dc133e188b3c79e7ec9147cd59693141e911534
GBachheimer <gbachheimer@icloud.com>
2022-06-22 17:05:09 +0300

    reset button and functionality added

--- generateButtons.js
+++ generateButtons.js
@@ -1,11 +1,11 @@
 function createButtons(number) {
-    var winner = Math.floor(Math.random() * number) + 1;
+    winner = Math.floor(Math.random() * number) + 1;
     for (let i = 1; i <= number; ++i) {
         let newButton = document.createElement("button");
         newButton.innerHTML = "Click me!";
         newButton.id = i;
-        newButton.classList = "btn btn-warning"
-        newButton.onclick = "checkWinner(this.id.value)";
+        newButton.classList = "btn btn-warning";
+        newButton.onclick = function () { checkWinner(this.id); };
         document.getElementById("createHere").appendChild(newButton);
     }
 }
