c7309ece3e00522f220606c30130272010e986f8
GBachheimer <gbachheimer@icloud.com>
2022-06-22 19:52:45 +0300

    put functions in one file

--- checkWinner.js
+++
@@ -1,9 +0,0 @@
-function checkWinner(id) {
-    const message = document.createElement("p");
-    if (winner == id) {
-        message.innerHTML = "You won!";
-    } else {
-        message.innerHTML = "You lose! I was behind button " + winner + ".";
-    }
-    document.getElementById("createHere").appendChild(message);
-}
--- functions.js
+++ functions.js
@@ -9,3 +9,13 @@ function createButtons(number) {
         document.getElementById("createHere").appendChild(newButton);
     }
 }
+
+function checkWinner(id) {
+    const message = document.createElement("p");
+    if (winner == id) {
+        message.innerHTML = "You won!";
+    } else {
+        message.innerHTML = "You lose! I was behind button " + winner + ".";
+    }
+    document.getElementById("createHere").appendChild(message);
+}
